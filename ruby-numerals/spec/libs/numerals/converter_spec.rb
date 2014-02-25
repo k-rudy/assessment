704f0d4a5b40d1bff81359a1f5ff9e5567a529fb
Konstantin Rudy <konstantin.rudy@gmail.com>
2014-02-25 10:54:39 +0300

    Described Converter logic

---
+++ ruby-numerals/spec/libs/numerals/converter_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Numerals::Converter do
+  
+  subject { Numerals::Converter }
+  
+  describe '#convert' do
+    
+    it 'converts long numbers correctly' 
+    
+    it 'converts non-integers correctly'
+      
+    it 'converts negatives correctly'
+    
+    it 'returns string itself when string is not a number'
+
+  end
+end
