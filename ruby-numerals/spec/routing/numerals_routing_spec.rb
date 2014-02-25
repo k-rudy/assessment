a4d95cb8b3438a05b908fd005ded2a0eb6857e11
Konstantin Rudy <konstantin.rudy@gmail.com>
2014-02-25 10:13:05 +0300

    Described numerals routing

---
+++ ruby-numerals/spec/routing/numerals_routing_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe NumeralsController do
+
+  describe "routing" do
+
+    it "routes GET /numerals to :index" do
+
+      expect(get('/numerals')).to route_to('numerals#index')
+    end
+
+    it "routes POST /numerals to :convert" do
+
+      expect(post('/numerals')).to route_to('numerals#convert')
+    end
+  end
+end
