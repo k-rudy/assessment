6d5a1f5159442a01a7aab291968acced770fd3c3
Konstantin Rudy <konstantin.rudy@gmail.com>
2014-02-25 11:42:53 +0300

    Routing spec styling adjusted

--- ruby-numerals/spec/routing/numerals_routing_spec.rb
+++ ruby-numerals/spec/routing/numerals_routing_spec.rb
@@ -5,12 +5,10 @@ describe NumeralsController do
   describe "routing" do
 
     it "routes GET /numerals to :index" do
-
       expect(get('/numerals')).to route_to('numerals#index')
     end
 
     it "routes POST /numerals/convert to :convert" do
-
       expect(post('/numerals/convert')).to route_to('numerals#convert')
     end
   end
