e2e3527833e00b714ea9232451096080bb0bb931
Konstantin Rudy <konstantin.rudy@gmail.com>
2014-02-25 09:57:35 +0300

    Initial rails application added

---
+++ ruby-numerals/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyNumerals::Application.config.secret_key_base = '4fca7207306de6ee87a77be3cc01b986b04267ba529e5b6e42281ef56c587ca480b0f6900da8a7239ea3ed29e28c46796db1b908ae384fc7e0e6608e72158f22'
